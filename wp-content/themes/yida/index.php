fe9ad65e66eb7271e5d7b992b9ceab370a56e261
kaiyu li <krisfk@gmail.com>
2021-02-20 23:55:49 +0800

    .

--- wp-content/themes/yida/index.php
+++ wp-content/themes/yida/index.php
@@ -721,11 +721,14 @@ get_template_directory_uri().'/images/logo.png'; ?>" alt="">
             <div id="js-index-slider1" class="p-index-slider" data-slide-time="7000">
                 <div class="p-index-slider__item p-index-slider__item--1 has-button has-link-button">
                     <a href="javascript:void(0);" class="p-index-slider__item__inner">
+                        <div class="p-index-slider__item-content">
+                            <div class="p-index-slider__item-content__inner l-inner">
+                            </div>
+                        </div>
+                        <div class="p-index-slider__item-image">
 
-                        <!-- <div class="p-index-slider__item-image"> -->
-
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/slide_01.jpg" alt="">
-                        <!-- </div> -->
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/slide_01.jpg" alt="">
+                        </div>
                     </a>
 
                 </div>
