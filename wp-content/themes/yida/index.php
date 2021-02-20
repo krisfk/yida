37149d92168ca15733d9f50f140005d5bc929014
kaiyu li <krisfk@gmail.com>
2021-02-20 19:10:07 +0800

    .

--- wp-content/themes/yida/index.php
+++ wp-content/themes/yida/index.php
@@ -821,7 +821,7 @@ get_template_directory_uri().'/images/logo.png'; ?>" alt="">
 
             <div class="container">
 
-                <div class="row">
+                <div class="row g-0">
 
                     <div class="col-lg-6">
 
