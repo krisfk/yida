79b67cd16f4b090510ed2c4fa0d6990ba8e8919d
kaiyu li <krisfk@gmail.com>
2021-02-20 23:06:38 +0800

    .

--- wp-content/themes/yida/index.php
+++ wp-content/themes/yida/index.php
@@ -839,6 +839,22 @@ get_template_directory_uri().'/images/home-thumb-1.jpg'; ?>" alt="">
 get_template_directory_uri().'/images/home-thumb-1.jpg'; ?>" alt="">
                         </a>
                     </div>
+                    <div class="col-lg-6">
+
+
+                        <a href="javascript:void(0);">
+                            <img class="w-100" src="<?php echo 
+get_template_directory_uri().'/images/home-thumb-1.jpg'; ?>" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+
+
+                        <a href="javascript:void(0);">
+                            <img class="w-100" src="<?php echo 
+get_template_directory_uri().'/images/home-thumb-1.jpg'; ?>" alt="">
+                        </a>
+                    </div>
                 </div>
             </div>
             <!-- <table class="mt-5 content-table">
