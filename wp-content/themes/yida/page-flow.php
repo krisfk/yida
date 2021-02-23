d5eefba97cdbc79d9a7b0dae6a1265b4d6434f12
kaiyu li <krisfk@gmail.com>
2021-02-24 03:45:45 +0800

    .

--- wp-content/themes/yida/page-flow.php
+++ wp-content/themes/yida/page-flow.php
@@ -120,7 +120,7 @@
                     <td></td>
                     <td class="pt-3 pb-5">
                         <img class="footer-grey-img" src="<?php echo 
-get_template_directory_uri().'/images/step1.png'; ?>">
+get_template_directory_uri().'/images/step-1.png'; ?>">
 
                     </td>
                 </tr>
