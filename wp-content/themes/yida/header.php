28f243d2821c7679496d1943c79b61fb75140ebd
kaiyu li <krisfk@gmail.com>
2021-02-24 03:08:29 +0800

    .

--- wp-content/themes/yida/header.php
+++ wp-content/themes/yida/header.php
@@ -691,7 +691,7 @@ get_template_directory_uri().'/images/logo.png'; ?>" alt="">
                             <a href="#" aria-current="page">工程案例<span></span></a>
                         </li>
                         <li id="menu-item-809"
-                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home page_item page-item-68  menu-item-809">
+                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home <?php echo get_the_slug()=='flow' ?  'current-menu-item' :'';?> page_item page-item-68  menu-item-809">
                             <a href="#" aria-current="page">裝修流程<span></span></a>
                         </li>
                         <li id="menu-item-809"
