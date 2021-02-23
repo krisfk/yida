1a2e450be14207d47cd18027e27e86c6af5a4996
kaiyu li <krisfk@gmail.com>
2021-02-24 05:19:42 +0800

    .

--- wp-content/themes/yida/footer.php
+++ wp-content/themes/yida/footer.php
@@ -1,74 +1,162 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
+<footer class="l-footer">
+    <div class="p-footer-search--mobile l-inner">
+        <form action="https://design-halue.com/" method="get">
+            <input type="text" name="s" value="" class="p-footer-search__input" placeholder="SEARCH">
+            <input type="submit" value="&#xe915;" class="p-footer-search__submit">
+        </form>
+    </div>
+    <div id="js-footer-widget" class="p-footer-widget-area p-footer-widget-area__has-social-nav">
+        <div class="p-footer-widget-area__inner l-inner">
+            <div class=" w-100 p-widget-footer site_info_widget pt-xl-5 pb-xl-5 pt-lg-5 pb-lg-5 pt-md-3 pb-md-3 pt-sm-3 pb-sm-3"
+                id="site_info_widget-3">
+                <div class="p-siteinfo">
+                    <!-- <div class="p-siteinfo__image p-siteinfo__logo"><img alt="inteior design halue"
+                                src="https://design-halue.com/wp-content/uploads/2020/02/logo.png"></div> -->
 
-?>
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	</div><!-- #content -->
 
-	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
+                    <div class="row g-0">
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+                        <div class="col-xl-3 col-md-4 my-auto footer-grey-text">我們是一家室內設計裝修公司， <br>
+                            可幫助您佈置生活，享受你所想，<br>
+                            並創造一個充滿能量的空間。</div>
+                        <div class="col-xl-9 col-md-8 footer-grey-img-div">
+                            <img class="footer-grey-img" src="<?php echo 
+get_template_directory_uri().'/images/footer-thumbnail-1.jpg'; ?>">
+                            <img class="footer-grey-img" src="<?php echo 
+get_template_directory_uri().'/images/footer-thumbnail-2.jpg'; ?>">
+                        </div>
 
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
-		<div class="site-info">
-			<div class="site-name">
-				<?php if ( has_custom_logo() ) : ?>
-					<div class="site-logo"><?php the_custom_logo(); ?></div>
-				<?php else : ?>
-					<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-						<?php if ( is_front_page() && ! is_paged() ) : ?>
-							<?php bloginfo( 'name' ); ?>
-						<?php else : ?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						<?php endif; ?>
-					<?php endif; ?>
-				<?php endif; ?>
-			</div><!-- .site-name -->
-			<div class="powered-by">
-				<?php
-				printf(
-					/* translators: %s: WordPress. */
-					esc_html__( 'Proudly powered by %s.', 'twentytwentyone' ),
-					'<a href="' . esc_url( __( 'https://wordpress.org/', 'twentytwentyone' ) ) . '">WordPress</a>'
-				);
-				?>
-			</div><!-- .powered-by -->
+                    </div>
 
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+                </div>
 
-</div><!-- #page -->
+            </div>
+        </div>
 
-<?php wp_footer(); ?>
+
+    </div>
+    </div>
+
+
+    </div>
+    </div>
+
+    <div class="l-inner  d-table pt-3 pb-3">
+
+        <div class="footer-info-1">
+
+
+            <h3>依達貿易有限公司</h3>
+
+            <div>
+                地址:香港九龍大角咀合桃街1號福昌工廠大廈6樓A5室　<br>
+                電話: 9316 6606 李生 傳真 : 8147 1279
+                <!-- Copyright © 2020 YI DA TRADING LIMITED. All Rights Reserved. -->
+            </div>
+        </div>
+        <div class="footer-info-2">注冊小型工程承辦商 注冊電業承辦商
+        </div>
+
+    </div>
+    <div class="p-copyright">
+        <div class="l-inner text-start">
+            Copyright © 2020 YI DA TRADING LIMITED. All Rights Reserved.
+        </div>
+    </div>
+    <div id="js-pagetop" class="p-pagetop"><a href="#"></a></div>
+</footer>
+
+
+<link rel='stylesheet' id='insta-gallery-css'
+    href='<?php echo get_template_directory_uri().'/yida-assets/'; ?>frontend.css?ver=3.1.0' type='text/css'
+    media='all' />
+<link rel='stylesheet' id='magnific-popup-css'
+    href='<?php echo get_template_directory_uri().'/yida-assets/'; ?>magnific-popup.min.css?ver=3.1.0' type='text/css'
+    media='all' />
+<link rel='stylesheet' id='swiper-css'
+    href='<?php echo get_template_directory_uri().'/yida-assets/'; ?>swiper.min.css?ver=3.1.0' type='text/css'
+    media='all' />
+<script type='text/javascript' id='contact-form-7-js-extra'>
+/* <![CDATA[ */
+var wpcf7 = {
+    "apiSettings": {
+        "root": "https:\/\/design-halue.com\/wp-json\/contact-form-7\/v1",
+        "namespace": "contact-form-7\/v1"
+    }
+};
+/* ]]> */
+</script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>scripts.js?ver=5.3'
+    id='contact-form-7-js'></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>front-page.js?ver=1.6'
+    id='iconic-front-page-js'></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>functions.js?ver=1.6'
+    id='iconic-script-js'>
+</script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>slick.min.js?ver=1.6'
+    id='iconic-slick-js'>
+</script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>header-fix.js?ver=1.6'
+    id='iconic-header-fix-js'></script>
+<script type='text/javascript'
+    src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>wp-embed.min.js?ver=5.5.3' id='wp-embed-js'>
+</script>
+<script type='text/javascript' id='insta-gallery-js-extra'>
+/* <![CDATA[ */
+var qligg = {
+    "ajax_url": "https:\/\/design-halue.com\/wp-admin\/admin-ajax.php"
+};
+/* ]]> */
+</script>
+<script type='text/javascript'
+    src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>frontend.js?ver=75cfaa425a2687569e77100aa3d2c489'
+    id='insta-gallery-js'></script>
+<script type='text/javascript'
+    src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>jquery.magnific-popup.min.js?ver=3.1.0'
+    id='magnific-popup-js'></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri().'/yida-assets/'; ?>swiper.min.js?ver=5.3.6'
+    id='swiper-js'>
+</script>
+<script>
+jQuery(function($) {
+    var initialized = false;
+    var initialize = function() {
+        if (initialized) return;
+        initialized = true;
+
+        $(document).trigger('js-initialized');
+        $(window).trigger('resize').trigger('scroll');
+    };
+
+    initialize();
+
+
+    var viewing_idx = 1;
+    $('.home-portfolio-filter-ul li a').click(function() {
+        $('.home-portfolio-filter-ul li a').removeClass('active');
+        $(this).toggleClass('active');
+
+        viewing_idx = $(this).parent('li').index() + 1;
+
+        $('.portfolio-thumbnail').fadeOut(0);
+        $('.portfolio-thumbnail-' + viewing_idx).fadeIn(0);
+
+        // $('.p-index-slider').fadeOut(0);
+
+        $('.p-index-slider').css({
+            'z-index': '0'
+        });
+
+        $('.p-index-slider#js-index-slider' + viewing_idx).css({
+            'z-index': '100'
+        })
+    })
+
+});
+</script>
+
 
 </body>
+
+
 </html>
