3fbd33db917fc173ea80242e4eccf55cbdd91105
kaiyu li <krisfk@gmail.com>
2021-02-24 05:17:19 +0800

    .

--- wp-content/themes/yida/page-flow.php
+++ wp-content/themes/yida/page-flow.php
@@ -44,45 +44,7 @@
         </div>
 
 
-        <div id="js-index-slider2" class="p-index-slider" data-slide-time="7000">
-            <div class="p-index-slider__item p-index-slider__item--1 has-button has-link-button">
-                <a href="javascript:void(0);" class="p-index-slider__item__inner">
-                    <div class="p-index-slider__item-content">
-                        <div class="p-index-slider__item-content__inner l-inner">
-                        </div>
-                    </div>
-                    <div class="p-index-slider__item-image">
 
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/test-banner.jpg" alt="">
-                    </div>
-                </a>
-
-            </div>
-            <div class="p-index-slider__item p-index-slider__item--2 has-button has-link-button">
-                <a href="javascript:void(0);" class="p-index-slider__item__inner">
-                    <div class="p-index-slider__item-content">
-                        <div class="p-index-slider__item-content__inner l-inner">
-                        </div>
-                    </div>
-                    <div class="p-index-slider__item-image">
-
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/test-banner.jpg" alt="">
-                    </div>
-                </a>
-            </div>
-            <div class="p-index-slider__item p-index-slider__item--3 has-button has-link-button">
-                <a href="javascript:void(0);" class="p-index-slider__item__inner">
-                    <div class="p-index-slider__item-content">
-                        <div class="p-index-slider__item-content__inner l-inner">
-                        </div>
-                    </div>
-                    <div class="p-index-slider__item-image">
-
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/test-banner.jpg" alt="">
-                    </div>
-                </a>
-            </div>
-        </div>
     </div>
 
 
