a202a3acdf9401378c96c5973909a129fb431ea0
kaiyu li <krisfk@gmail.com>
2021-02-24 02:59:35 +0800

    .

--- wp-content/themes/yida/page-flow.php
+++ wp-content/themes/yida/page-flow.php
@@ -89,101 +89,10 @@
     <div class="l-inner position-relative pt-5">
 
 
-        <ul class="home-portfolio-filter-ul">
-            <li><a class="active" href="javascript:void(0);"> 精緻作品 <span class="arrow">&gt;</span></a></li>
-            <li><a href="javascript:void(0);">實用作品 <span class="arrow">&gt;</span></a></li>
-        </ul>
 
 
-        <div class="container g-0 portfolio-thumbnail portfolio-thumbnail-1">
 
-            <div class="row">
-
-                <div class="col-lg-6 mb-4">
-
-                    <a href="javascript:void(0);">
-                        <img class="w-100" src="<?php echo 
-get_template_directory_uri().'/images/home-thumb-1.jpg'; ?>" alt="">
-                    </a>
-
-                </div>
-                <div class="col-lg-6 mb-4">
-
-
-                    <a href="javascript:void(0);">
-                        <img class="w-100" src="<?php echo 
-get_template_directory_uri().'/images/home-thumb-2.jpg'; ?>" alt="">
-                    </a>
-                </div>
-                <div class="col-lg-6 mb-4">
-
-
-                    <a href="javascript:void(0);">
-                        <img class="w-100" src="<?php echo 
-get_template_directory_uri().'/images/home-thumb-3.jpg'; ?>" alt="">
-                    </a>
-                </div>
-                <div class="col-lg-6 mb-4">
-
-
-                    <a href="javascript:void(0);">
-                        <img class="w-100" src="<?php echo 
-get_template_directory_uri().'/images/home-thumb-4.jpg'; ?>" alt="">
-                    </a>
-                </div>
-
-            </div>
-        </div>
-
-
-        <div class="container g-0 portfolio-thumbnail portfolio-thumbnail-2">
-
-            <div class="row">
-
-                <div class="col-lg-6 mb-4">
-
-                    <a href="javascript:void(0);">
-
-                        <div class="caption">The Rednaxela 帝華臺</div>
-                        <img class="w-100" src="<?php echo 
-get_template_directory_uri().'/images/home-thumb-4.jpg'; ?>" alt="">
-                    </a>
-
-                </div>
-                <div class="col-lg-6 mb-4">
-
-                    <a href="javascript:void(0);">
-
-                        <div class="caption">The Rednaxela 帝華臺</div>
-                        <img class="w-100" src="<?php echo 
-get_template_directory_uri().'/images/home-thumb-4.jpg'; ?>" alt="">
-                    </a>
-
-                </div>
-                <div class="col-lg-6 mb-4">
-
-                    <a href="javascript:void(0);">
-
-                        <div class="caption">The Rednaxela 帝華臺</div>
-                        <img class="w-100" src="<?php echo 
-get_template_directory_uri().'/images/home-thumb-4.jpg'; ?>" alt="">
-                    </a>
-
-                </div>
-                <div class="col-lg-6 mb-4">
-
-                    <a href="javascript:void(0);">
-
-                        <div class="caption">The Rednaxela 帝華臺</div>
-                        <img class="w-100" src="<?php echo 
-get_template_directory_uri().'/images/home-thumb-4.jpg'; ?>" alt="">
-                    </a>
-
-                </div>
-
-            </div>
-        </div>
-
+        fdfs
 
 
 
