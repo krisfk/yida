c6ebe0ea1a061dc7dd9cf23e3bea361ad4b0579c
kaiyu li <krisfk@gmail.com>
2021-02-25 04:57:59 +0800

    .

--- wp-content/themes/yida/single-work.php
+++ wp-content/themes/yida/single-work.php
@@ -109,10 +109,18 @@ $(function() {
         delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image',
         gallery: {
-            tPrev: 'Previous (Left arrow key)',
-            tNext: 'Next (Right arrow key)',
-            tCounter: '%curr% of %total%'
-        },
+            enabled: false, // set to true to enable gallery
+
+            preload: [0, 2], // read about this option in next Lazy-loading section
+
+            navigateByImgClick: true,
+
+            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+            tPrev: 'Previous (Left arrow key)', // title for left button
+            tNext: 'Next (Right arrow key)', // title for right button
+            tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+        }
         // other options
     });
 
