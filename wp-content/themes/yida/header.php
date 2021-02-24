1acdcb25bfba819a6c59ff1aa74e313f6f4862c8
kaiyu li <krisfk@gmail.com>
2021-02-24 15:44:06 +0800

    .

--- wp-content/themes/yida/header.php
+++ wp-content/themes/yida/header.php
@@ -641,11 +641,11 @@
     <style type="text/css">
 
     </style>
-    <link rel="icon" href="https://design-halue.com/wp-content/uploads/2020/02/favicon-150x150.png" sizes="32x32" />
-    <link rel="icon" href="https://design-halue.com/wp-content/uploads/2020/02/favicon-300x300.png" sizes="192x192" />
-    <link rel="apple-touch-icon" href="https://design-halue.com/wp-content/uploads/2020/02/favicon-300x300.png" />
+    <link rel="icon" href="http://167.71.95.220/wp-content/uploads/2021/02/yida-ico@150x150.jpg" sizes="32x32" />
+    <link rel="icon" href="http://167.71.95.220/wp-content/uploads/2021/02/yida-ico@300x300.jpg" sizes="192x192" />
+    <link rel="apple-touch-icon" href="http://167.71.95.220/wp-content/uploads/2021/02/yida-ico@300x300.jpg" />
     <meta name="msapplication-TileImage"
-        content="https://design-halue.com/wp-content/uploads/2020/02/favicon-300x300.png" />
+        content="http://167.71.95.220/wp-content/uploads/2021/02/yida-ico@300x300.jpg" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
