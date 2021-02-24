c339492a227a1ac6b5a09d17d480ed152e38bdda
kaiyu li <krisfk@gmail.com>
2021-02-25 05:23:21 +0800

    .

--- wp-content/themes/yida/single-work.php
+++ wp-content/themes/yida/single-work.php
@@ -103,6 +103,19 @@
 
 <script type="text/javascript">
 var $ = jQuery;
+
+
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+
 $(function() {
 
     $('.gallery-container').magnificPopup({
@@ -149,6 +162,18 @@ $(function() {
         //.addClass('animate');
     }
     // $('..work-photo-a')
+
+
+
+
+    $(window).on('resize scroll', function() {
+        if ($('.gallery-container').isInViewport()) {
+            console.log('in viewport');
+        } else {
+            console.log('not in viewport');
+        }
+    });
+
 
 
 })
