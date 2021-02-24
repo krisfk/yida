658c123c568dffabf8034d3f7135e0347bf2d93b
kaiyu li <krisfk@gmail.com>
2021-02-25 05:04:01 +0800

    .

--- wp-content/themes/yida/single-work.php
+++ wp-content/themes/yida/single-work.php
@@ -105,29 +105,43 @@
 var $ = jQuery;
 $(function() {
 
-    $('.gallery-container').magnificPopup({
-        delegate: 'a', // child items selector, by clicking on it popup will open
-        type: 'image',
-        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+            $('.gallery-container').magnificPopup({
+                    delegate: 'a', // child items selector, by clicking on it popup will open
+                    type: 'image',
+                    mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+                    zoom: {
+                        enabled: true, // By default it's false, so don't forget to enable it
 
-        gallery: {
-            enabled: true, // set to true to enable gallery
+                        duration: 300, // duration of the effect, in milliseconds
+                        easing: 'ease-in-out', // CSS transition easing function
 
-            preload: [0, 2], // read about this option in next Lazy-loading section
+                        // The "opener" function should return the element from which popup will be zoomed in
+                        // and to which popup will be scaled down
+                        // By defailt it looks for an image tag:
+                        opener: function(openerElement) {
+                            // openerElement is the element on which popup was initialized, in this case its <a> tag
+                            // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                            return openerElement.is('img') ? openerElement : openerElement.find('img');
+                        },
 
-            navigateByImgClick: true,
+                        gallery: {
+                            enabled: true, // set to true to enable gallery
 
-            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+                            preload: [0, 2], // read about this option in next Lazy-loading section
 
-            tPrev: 'Previous (Left arrow key)', // title for left button
-            tNext: 'Next (Right arrow key)', // title for right button
-            tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-        }
-        // other options
-    });
+                            navigateByImgClick: true,
+
+                            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+                            tPrev: 'Previous (Left arrow key)', // title for left button
+                            tNext: 'Next (Right arrow key)', // title for right button
+                            tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+                        }
+                        // other options
+                    });
 
 
-})
+            })
 </script>
 <?php 
 get_footer();?>
