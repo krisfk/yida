65b8e4573a04e697f8bd98bdf1343cb76cfadfee
kaiyu li <krisfk@gmail.com>
2021-02-25 03:32:40 +0800

    .

--- wp-content/themes/yida/single-work.php
+++ wp-content/themes/yida/single-work.php
@@ -77,7 +77,7 @@
 
                             ?>
 
-                        <a href="javascript:void(0);" class="work-photo-a"><img src="<?php echo $img_url_full;?>"
+                        <a href="javascript:void(0);" class="work-photo-a"><img src="<?php echo $img_url_thumb;?>"
                                 alt=""></a>
                         <?php
                             // echo $img_url.'<br/>';
