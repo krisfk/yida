08db4bd9a034fb5181ea474ffdb5e7e92cfd6fa7
kaiyu li <krisfk@gmail.com>
2021-02-25 05:00:15 +0800

    .

--- wp-content/themes/yida/single-work.php
+++ wp-content/themes/yida/single-work.php
@@ -106,38 +106,38 @@ var $ = jQuery;
 $(function() {
 
             $('.gallery-container').magnificPopup({
-                    delegate: 'a', // child items selector, by clicking on it popup will open
-                    type: 'image',
-                    gallery: {
-                        enabled: true, // set to true to enable gallery
+                delegate: 'a', // child items selector, by clicking on it popup will open
+                type: 'image',
+                gallery: {
+                    enabled: true, // set to true to enable gallery
 
-                        preload: [0, 2], // read about this option in next Lazy-loading section
+                    preload: [0, 2], // read about this option in next Lazy-loading section
 
-                        navigateByImgClick: true,
+                    navigateByImgClick: true,
 
-                        arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+                    arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
 
-                        tPrev: 'Previous (Left arrow key)', // title for left button
-                        tNext: 'Next (Right arrow key)', // title for right button
-                        tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-                    },
-                    mainClass: 'mfp-with-zoom', // this class is for CSS animation below
-                    zoom: {
-                        enabled: true, // By default it's false, so don't forget to enable it
+                    tPrev: 'Previous (Left arrow key)', // title for left button
+                    tNext: 'Next (Right arrow key)', // title for right button
+                    tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+                },
+                mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+                zoom: {
+                    enabled: true, // By default it's false, so don't forget to enable it
 
-                        duration: 300, // duration of the effect, in milliseconds
-                        easing: 'ease-in-out', // CSS transition easing function
+                    duration: 300, // duration of the effect, in milliseconds
+                    easing: 'ease-in-out', // CSS transition easing function
 
-                        // The "opener" function should return the element from which popup will be zoomed in
-                        // and to which popup will be scaled down
-                        // By defailt it looks for an image tag:
-                        opener: function(openerElement) {
-                            // openerElement is the element on which popup was initialized, in this case its <a> tag
-                            // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                            return openerElement.is('img') ? openerElement : openerElement.find('img');
-                        }
-                        // other options
-                    });
+                    // The "opener" function should return the element from which popup will be zoomed in
+                    // and to which popup will be scaled down
+                    // By defailt it looks for an image tag:
+                    opener: function(openerElement) {
+                        // openerElement is the element on which popup was initialized, in this case its <a> tag
+                        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                        return openerElement.is('img') ? openerElement : openerElement.find('img');
+                    }
+                    // other options
+                }
 
 
             })
