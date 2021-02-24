3b91747bfbfbe2f28821689ff28dc4dda87e62f3
kaiyu li <krisfk@gmail.com>
2021-02-25 06:18:25 +0800

    .

--- wp-content/themes/yida/page-home.php
+++ wp-content/themes/yida/page-home.php
@@ -6,7 +6,7 @@
 
 
         <div id="js-index-slider1" class="p-index-slider" data-slide-time="7000">
-            <!-- <div class="dots-cover"></div> -->
+            <div class="dots-cover"></div>
 
 
             <?php
