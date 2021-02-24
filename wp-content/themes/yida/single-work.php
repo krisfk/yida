f5c9b1432678cc15af4d0b71caa06cd1c4ac3ed1
kaiyu li <krisfk@gmail.com>
2021-02-25 05:01:39 +0800

    .

--- wp-content/themes/yida/single-work.php
+++ wp-content/themes/yida/single-work.php
@@ -105,25 +105,27 @@
 var $ = jQuery;
 $(function() {
 
-            $('.gallery-container').magnificPopup({
-                delegate: 'a', // child items selector, by clicking on it popup will open
-                type: 'image',
-                gallery: {
-                    enabled: true, // set to true to enable gallery
+    $('.gallery-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true, // set to true to enable gallery
 
-                    preload: [0, 2], // read about this option in next Lazy-loading section
+            preload: [0, 2], // read about this option in next Lazy-loading section
 
-                    navigateByImgClick: true,
+            navigateByImgClick: true,
 
-                    arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
 
-                    tPrev: 'Previous (Left arrow key)', // title for left button
-                    tNext: 'Next (Right arrow key)', // title for right button
-                    tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-                }
+            tPrev: 'Previous (Left arrow key)', // title for left button
+            tNext: 'Next (Right arrow key)', // title for right button
+            tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+        }
+        // other options
+    });
 
 
-            })
+})
 </script>
 <?php 
 get_footer();?>
