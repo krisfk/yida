4ccf687523ab8e9b4de2e45aa61709e49dc7de24
kaiyu li <krisfk@gmail.com>
2021-02-25 03:19:21 +0800

    .

--- wp-content/themes/yida/single-work.php
+++ wp-content/themes/yida/single-work.php
@@ -3,31 +3,43 @@
 
     <div class="slide-container">
         <div id="js-index-slider1" class="p-index-slider" data-slide-time="7000">
-            <div class="slide-container">
-                <div id="js-index-slider1" class="p-index-slider" data-slide-time="7000">
-                    <div class="p-index-slider__item p-index-slider__item--1 has-button has-link-button">
-                        <div class="p-index-slider__item__inner">
-                            <div class="p-index-slider__item-content">
-                                <div class="p-index-slider__item-content__inner l-inner">
-                                </div>
-                            </div>
-                            <div class="p-index-slider__item-image">
-
-                                <?php
-                    $img_id = get_field( "banner" );
-                    $img_arr =wp_get_attachment_image_src($img_id,'full');
-                    $img_url = $img_arr[0];
-                    ?>
-                                <img src="<?php echo $img_url;?>" alt="">
-                            </div>
+            <div class="p-index-slider__item p-index-slider__item--1 has-button has-link-button">
+                <a href="javascript:void(0);" class="p-index-slider__item__inner">
+                    <div class="p-index-slider__item-content">
+                        <div class="p-index-slider__item-content__inner l-inner">
                         </div>
-
                     </div>
+                    <div class="p-index-slider__item-image">
 
-                </div>
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/slide_01.jpg" alt="">
+                    </div>
+                </a>
 
+            </div>
+            <div class="p-index-slider__item p-index-slider__item--2 has-button has-link-button">
+                <a href="javascript:void(0);" class="p-index-slider__item__inner">
+                    <div class="p-index-slider__item-content">
+                        <div class="p-index-slider__item-content__inner l-inner">
+                        </div>
+                    </div>
+                    <div class="p-index-slider__item-image">
 
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/slide_02.jpg" alt="">
+                    </div>
+                </a>
 
+            </div>
+            <div class="p-index-slider__item p-index-slider__item--3 has-button has-link-button">
+                <a href="javascript:void(0);" class="p-index-slider__item__inner">
+                    <div class="p-index-slider__item-content">
+                        <div class="p-index-slider__item-content__inner l-inner">
+                        </div>
+                    </div>
+                    <div class="p-index-slider__item-image">
+
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/slide_03.jpg" alt="">
+                    </div>
+                </a>
             </div>
         </div>
 
