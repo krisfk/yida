fbe8e83be76217692d91fc1f17a617bf0d462e88
kaiyu li <krisfk@gmail.com>
2021-02-26 05:07:09 +0800

    .

--- wp-content/themes/yida/header.php
+++ wp-content/themes/yida/header.php
@@ -676,7 +676,7 @@ get_template_directory_uri().'/common.js'; ?>"></script>
     <script src="https://kit.fontawesome.com/64716b40d8.js" crossorigin="anonymous"></script>
 
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
 </head>
 
--- wp-content/themes/yida/page-flow.php
+++ wp-content/themes/yida/page-flow.php
@@ -76,7 +76,7 @@
 
                     <td colspan="2" class="pt-3 pb-5">
 
-                        <h1 class="animated fadeIn">Animation</h1>
+                        <h1 class="animate__animated animate__fadeIn">Animation</h1>
 
                         <div class="  container-fluid flow-step-div flow-step-div-1">
 
