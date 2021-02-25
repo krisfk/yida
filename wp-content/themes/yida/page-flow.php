61a37c55d79e3599a76ecd2aeaaddcd0610c7801
kaiyu li <krisfk@gmail.com>
2021-02-26 05:00:31 +0800

    .

--- wp-content/themes/yida/page-flow.php
+++ wp-content/themes/yida/page-flow.php
@@ -210,10 +210,10 @@ $(function() {
                 $('.flow-step-div-1').addClass('fadeInLeft');
             }, 4000);
             $(window).on('resize scroll', function() {
-                    // if ($('.flow-step-div-1').isInViewport() && !$('.flow-step-div-1').hasClass('animate')) {
-                    // $('.flow-step-div-1').addClass('animate');
-                    // $('.flow-step-div-1').addClass('fadeInLeft');
-                }
+                // if ($('.flow-step-div-1').isInViewport() && !$('.flow-step-div-1').hasClass('animate')) {
+                // $('.flow-step-div-1').addClass('animate');
+                // $('.flow-step-div-1').addClass('fadeInLeft');
+
                 // });
 
 
