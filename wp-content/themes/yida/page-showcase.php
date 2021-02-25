3c39be608a31108186c1f33d83c81b937eddbb8e
kaiyu li <krisfk@gmail.com>
2021-02-26 03:50:47 +0800

    .

--- wp-content/themes/yida/page-showcase.php
+++ wp-content/themes/yida/page-showcase.php
@@ -133,6 +133,8 @@ $img_url = $img_arr[0];
 echo ' <div class="col-6 col-lg-3 mb-4">
 
 <a href="'.get_permalink().'">
+<div class="caption">'.get_the_title().'</div>
+
 <img class="w-100" src="'.$img_url.'" alt="">
     </a>
 
