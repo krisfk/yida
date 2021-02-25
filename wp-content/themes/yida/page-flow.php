3bc3ec9e23a72125b77c42cea275f39c12917379
kaiyu li <krisfk@gmail.com>
2021-02-26 04:48:36 +0800

    .

--- wp-content/themes/yida/page-flow.php
+++ wp-content/themes/yida/page-flow.php
@@ -200,9 +200,9 @@ get_template_directory_uri().'/images/step-3.png'; ?>"></div>
 <script type="text/javascript">
 $(function() {
 
-    // $('.flow-step-div-1').css({
-    //     'left': '-20px'
-    // });
+    $('.flow-step-div-1').css({
+        'left': '-20px'
+    });
     $(window).on('resize scroll', function() {
         if ($('.flow-step-div-1').isInViewport() && !$('.flow-step-div-1').hasClass('animate')) {
             $('.flow-step-div-1').addClass('animate');
