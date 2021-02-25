439cb1596eb3249ccecd1287835f873bc2bbf052
kaiyu li <krisfk@gmail.com>
2021-02-26 05:13:00 +0800

    .

--- wp-content/themes/yida/page-flow.php
+++ wp-content/themes/yida/page-flow.php
@@ -214,6 +214,9 @@ $(function() {
                 'animate__animated')) {
             $('.flow-step-div-1').addClass('animate__animated');
             $('.flow-step-div-1').addClass('animate__fadeInLeft');
+            $('.flow-step-div-1').addClass('animate__delay-2s');
+
+
         }
 
     });
