dbaaefea74686c730086418e0acebb986a37a26d
kaiyu li <krisfk@gmail.com>
2021-03-02 22:19:33 +0800

    .

--- wp-content/themes/yida/page-contact.php
+++ wp-content/themes/yida/page-contact.php
@@ -63,6 +63,11 @@
 
                         裝修工程種類繁多 , 不能盡錄 , 本公司提供免費報價及上門度尺 , 歡迎聯絡洽談 , 客戶亦可電郵圖則等資料查詢報價 <br><br>
 
+
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.81477434848!2d114.16055751528096!3d22.322843847716413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x340400b73904bceb%3A0x5e1be8061fbfd494!2z5aSn6KeS5ZKA5ZCI5qGD6KGXMeiZn-emj-aYjOW3peW7oOWkp-W7iA!5e0!3m2!1szh-TW!2shk!4v1614694761106!5m2!1szh-TW!2shk"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
                         <table class="d-inline-block">
                             <tr>
                                 <td class="pe-2">電話:</td>
